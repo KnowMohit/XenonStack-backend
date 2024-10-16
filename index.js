96ba582ffab9b57223492a669ccb7feb826567c2
KnowMohit <mkg251412@gmail.com>
2024-10-16 21:23:44 +0530

    Fix: Add dotenv package

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://vanshnarang49906:1234VANSH@cluster0.mugvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(
+    "mongodb+srv://mkg251412:C23xOV48GXcOdYlZ@urbanease.wu1de.mongodb.net/UrbanEase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/properties', require('./routes/properties'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/properties", require("./routes/properties"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
